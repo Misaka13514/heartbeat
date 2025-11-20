7b16deac0ddf9ba85ae9321d9345c6f3605466ce
Misaka13514 <Misaka13514@gmail.com>
2025-11-20 14:54:59 +0800

    Update config

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,107 +1,248 @@
-// This is a simplified example config file for quickstart
-// Some not frequently used features are omitted/commented out here
-// For a full-featured example, please refer to `uptime.config.full.ts`
-
-// Don't edit this line
 import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
 
 const pageConfig: PageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "Misaka13514's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+    { link: 'https://github.com/Misaka13514/heartbeat', label: 'GitHub' },
   ],
+  // [OPTIONAL] Group your monitors
+  // If not specified, all monitors will be shown in a single list
+  // If specified, monitors will be grouped and ordered, not-listed monitors will be invisble (but still monitored)
+  group: {
+    '🐧 Apeiria Arch Linux Repository': [
+      'apeiria_aur_index',
+      'apeiria_aur_build',
+    ],
+    '🍂 Proj. One Among Us': [
+      'oau_memo_frontend',
+      'oau_memo_data',
+      'oau_memo_backend',
+      'oau_about',
+      'oau_neorle',
+      'oau_falling_frontend',
+      'oau_falling_data',
+      'oau_matrix',
+      'oau_headscale',
+      'oau_liveweb',
+      'oau_tia',
+      'oau_tia_uniguide',
+    ],
+  },
+  // [OPTIONAL] Set the path to your favicon, default to '/favicon.png' if not specified
+  // favicon: 'https://example.com/favicon.ico',
+  // [OPTIONAL] Set the path to your logo, default to '/logo.svg' if not specified
+  // logo: 'https://example.com/logo.svg',
+  // [OPTIONAL] Maintenance related settings
+  maintenances: {
+    // [OPTIONAL] The color of upcoming maintenance alerts, default to 'gray'
+    // Active alerts will always use the color specified in the MaintenanceConfig
+    upcomingColor: 'gray',
+  },
+  // [OPTIONAL] Custom footer html
+  // customFooter: '',
 }
 
 const workerConfig: WorkerConfig = {
+  // [Optional] Write KV at most every N minutes unless the status changed, default to 3
+  kvWriteCooldownMinutes: 3,
+  // Enable HTTP Basic auth for status page & API by uncommenting the line below, format `<USERNAME>:<PASSWORD>`
+  // passwordProtection: 'username:password',
   // Define all your monitors here
   monitors: [
-    // Example HTTP Monitor
     {
-      // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'foo_monitor',
-      // `name` is used at status page and callback message
-      name: 'My API Monitor',
-      // `method` should be a valid HTTP Method
+      id: 'apeiria_aur_index',
+      name: 'apeiria: Index',
       method: 'GET',
-      // `target` is a valid URL
-      target: 'https://example.com',
-      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
-      tooltip: 'This is a tooltip for this monitor',
-      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-      statusPageLink: 'https://example.com',
-      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
-      expectedCodes: [200],
-      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
-      timeout: 10000,
-      // [OPTIONAL] headers to be sent
-      headers: {
-        'User-Agent': 'Uptimeflare',
-        Authorization: 'Bearer YOUR_TOKEN_HERE',
-      },
-      // [OPTIONAL] body to be sent (require POST/PUT/PATCH method)
-      // body: 'Hello, world!',
-      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
-      // responseKeyword: 'success',
-      // [OPTIONAL] if specified, the response must NOT contains the keyword to be considered as operational.
-      // responseForbiddenKeyword: 'bad gateway',
-      // [OPTIONAL] if specified, will call the check proxy to check the monitor, mainly for geo-specific checks
-      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Check-proxy-setup before setting this value
-      // currently supports `worker://`, `globalping://` and `http(s)://` proxies
-      // checkProxy: 'worker://weur',
-      // [OPTIONAL] if true, the check will fallback to local if the specified proxy is down
-      // checkProxyFallback: true,
+      target: 'https://aur.apeiria.net',
+      tooltip: 'Cloudflare Workers + OneDrive',
+      statusPageLink: 'https://github.com/Misaka13514-AUR/ODIndex',
+      responseKeyword: 'Apeiria Arch Linux Repository',
+      checkProxy: 'worker://apac',
+      checkProxyFallback: true,
     },
-    // Example TCP Monitor
     {
-      id: 'test_tcp_monitor',
-      name: 'Example TCP Monitor',
-      // `method` should be `TCP_PING` for tcp monitors
-      method: 'TCP_PING',
-      // `target` should be `host:port` for tcp monitors
-      target: '1.2.3.4:22',
-      tooltip: 'My production server SSH',
-      statusPageLink: 'https://example.com',
-      timeout: 5000,
+      id: 'apeiria_aur_build',
+      name: 'apeiria: Build Logs',
+      method: 'GET',
+      target: 'https://build.apeiria.net',
+      tooltip: 'Cloudflare Workers + Supabase',
+      statusPageLink: 'https://github.com/Misaka13514-AUR/build-logs',
+      responseKeyword: 'Packages',
+      checkProxy: 'worker://apac',
+      checkProxyFallback: true,
+    },
+    {
+      id: 'oau_memo_frontend',
+      name: 'OAU: memorial website - Frontend',
+      method: 'GET',
+      target: 'https://one-among.us/?uwu=true',
+      tooltip: 'GitHub Pages + Cloudflare CDN',
+      statusPageLink: 'https://github.com/one-among-us/web',
+      responseKeyword: '那些秋叶',
+      checkProxy: 'worker://apac',
+      checkProxyFallback: true,
+    },
+    {
+      id: 'oau_memo_data',
+      name: 'OAU: memorial website - Data',
+      method: 'GET',
+      target: 'https://data.one-among.us/eggs.json',
+      tooltip: 'GitHub Pages + Cloudflare CDN',
+      statusPageLink: 'https://github.com/one-among-us/data',
+      responseKeyword: '舞萌',
+      checkProxy: 'worker://apac',
+      checkProxyFallback: true,
+    },
+    {
+      id: 'oau_memo_backend',
+      name: 'OAU: memorial website - Backend',
+      method: 'GET',
+      target: 'https://backend.one-among.us/flowers/get?id=Misaka13514',
+      tooltip: 'Vultr (Seattle) + Cloudflare CDN',
+      statusPageLink: 'https://github.com/one-among-us/backend',
+      responseKeyword: '0',
+      checkProxy: 'worker://apac',
+      checkProxyFallback: true,
+    },
+    {
+      id: 'oau_about',
+      name: 'OAU: about website',
+      method: 'GET',
+      target: 'https://oneamongus.ca',
+      tooltip: 'GitHub Pages + Cloudflare CDN',
+      statusPageLink: 'https://github.com/one-among-us/about-site',
+      responseKeyword: 'One Among Us Transgender Support',
+      checkProxy: 'worker://apac',
+      checkProxyFallback: true,
+    },
+    {
+      id: 'oau_neorle',
+      name: 'OAU: neo-RLEwiki',
+      method: 'GET',
+      target: 'https://rle.oau.app',
+      tooltip: 'GitHub Pages + Cloudflare CDN',
+      statusPageLink: 'https://github.com/one-among-us/neo-RLEwiki',
+      responseKeyword: 'neo-RLEwiki',
+      checkProxy: 'worker://apac',
+      checkProxyFallback: true,
+    },
+    {
+      id: 'oau_falling_frontend',
+      name: 'OAU: Falling Like - Frontend',
+      method: 'GET',
+      target: 'https://f.oneamongus.ca',
+      tooltip: 'GitHub Pages + Cloudflare CDN',
+      statusPageLink: 'https://github.com/one-among-us/falling-like',
+      responseKeyword: 'What If You Are Falling',
+      checkProxy: 'worker://apac',
+      checkProxyFallback: true,
+    },
+    {
+      id: 'oau_falling_data',
+      name: 'OAU: Falling Like - Data',
+      method: 'GET',
+      target: 'https://eu2.contabostorage.com/176b8434127b4d90bfb1360e128e586c:oneamongus/falling-data/questions.json',
+      tooltip: 'Contabo S3 (European Union)',
+      statusPageLink: 'https://github.com/one-among-us/falling-like/blob/master/src/logic/config.ts',
+      responseKeyword: 'Play_With_Cats',
+      checkProxy: 'worker://apac',
+      checkProxyFallback: true,
+    },
+    {
+      id: 'oau_matrix',
+      name: 'OAU: Matrix',
+      method: 'GET',
+      target: 'https://oau.app/.well-known/matrix/client',
+      tooltip: 'Contabo (Nuremberg) + Cloudflare CDN',
+      statusPageLink: 'https://github.com/one-among-us/appservers/blob/main/apps/matrix.nix',
+      responseKeyword: 'm.homeserver',
+      checkProxy: 'worker://apac',
+      checkProxyFallback: true,
+    },
+    {
+      id: 'oau_headscale',
+      name: 'OAU: Headscale',
+      method: 'GET',
+      target: 'https://hs.oau.app/windows',
+      tooltip: 'Contabo (Nuremberg) + Cloudflare CDN',
+      statusPageLink: 'https://github.com/one-among-us/appservers/blob/main/apps/headscale.nix',
+      responseKeyword: 'headscale: Windows configuration',
+      checkProxy: 'worker://apac',
+      checkProxyFallback: true,
+    },
+    {
+      id: 'oau_liveweb',
+      name: 'OAU: live redirector',
+      method: 'GET',
+      target: 'https://live.oau.app',
+      tooltip: 'Contabo (Nuremberg) + Cloudflare CDN',
+      statusPageLink: 'https://github.com/one-among-us/appservers/blob/main/apps/live-website/index.html',
+      responseKeyword: '直播已经结束，点击下列链接查看回放',
+      checkProxy: 'worker://apac',
+      checkProxyFallback: true,
+    },
+    {
+      id: 'oau_tia',
+      name: 'OAU: Tia redirector',
+      method: 'GET',
+      target: 'https://oau.edu.kg',
+      tooltip: 'Contabo (Nuremberg) + Cloudflare CDN',
+      statusPageLink: 'https://github.com/one-among-us/appservers/blob/main/apps/tia-legacy/index.html',
+      responseKeyword: 'Redirecting to One Among Us homepage',
+      checkProxy: 'worker://apac',
+      checkProxyFallback: true,
+    },
+    {
+      id: 'oau_tia_uniguide',
+      name: 'OAU: UniGuide',
+      method: 'GET',
+      target: 'https://uniguide.oau.edu.kg',
+      tooltip: 'GitHub Pages + Cloudflare CDN',
+      statusPageLink: 'https://github.com/one-among-us/TransAcademicUniGuide',
+      responseKeyword: 'Transgender University Guidance',
+      checkProxy: 'worker://apac',
+      checkProxyFallback: true,
     },
   ],
-  // [Optional] Notification settings
   notification: {
-    // [Optional] Notification webhook settings, if not specified, no notification will be sent
-    // More info at Wiki: https://github.com/lyc8503/UptimeFlare/wiki/Setup-notification
     webhook: {
-      // [Required] webhook URL (example: Telegram Bot API)
-      url: 'https://api.telegram.org/bot123456:ABCDEF/sendMessage',
-      // [Optional] HTTP method, default to 'GET' for payloadType=param, 'POST' otherwise
-      // method: 'POST',
-      // [Optional] headers to be sent
-      // headers: {
-      //   foo: 'bar',
-      // },
-      // [Required] Specify how to encode the payload
-      // Should be one of 'param', 'json' or 'x-www-form-urlencoded'
-      // 'param': append url-encoded payload to URL search parameters
-      // 'json': POST json payload as body, set content-type header to 'application/json'
-      // 'x-www-form-urlencoded': POST url-encoded payload as body, set content-type header to 'x-www-form-urlencoded'
-      payloadType: 'x-www-form-urlencoded',
-      // [Required] payload to be sent
-      // $MSG will be replaced with the human-readable notification message
+      // Don't mess with my telegram bot pls uwu :D
+      url: 'https://api.telegram.org/bot8249335103:AAHJ3FWf-BWSFSJ__I7r16hlU7-SJjhjfk8/sendMessage',
+      payloadType: 'param',
       payload: {
-        chat_id: 12345678,
+        chat_id: 673435039,
         text: '$MSG',
       },
-      // [Optional] timeout calling this webhook, in millisecond, default to 5000
-      timeout: 10000,
     },
-    // [Optional] timezone used in notification messages, default to "Etc/GMT"
     timeZone: 'Asia/Shanghai',
-    // [Optional] grace period in minutes before sending a notification
-    // notification will be sent only if the monitor is down for N continuous checks after the initial failure
-    // if not specified, notification will be sent immediately
     gracePeriod: 5,
+  },
+  callbacks: {
+    onStatusChange: async (
+      env: any,
+      monitor: any,
+      isUp: boolean,
+      timeIncidentStart: number,
+      timeNow: number,
+      reason: string
+    ) => {
+      // This callback will be called when there's a status change for any monitor
+      // Write any TypeScript code here
+      // This will not follow the grace period settings and will be called immediately when the status changes
+      // You need to handle the grace period manually if you want to implement it
+    },
+    onIncident: async (
+      env: any,
+      monitor: any,
+      timeIncidentStart: number,
+      timeNow: number,
+      reason: string
+    ) => {
+      // This callback will be called EVERY 1 MINTUE if there's an on-going incident for any monitor
+      // Write any TypeScript code here
+    },
   },
 }
 
@@ -109,26 +250,8 @@ const workerConfig: WorkerConfig = {
 // During maintenance, an alert will be shown at status page
 // Also, related downtime notifications will be skipped (if any)
 // Of course, you can leave it empty if you don't need this feature
-
 // const maintenances: MaintenanceConfig[] = []
+const maintenances: MaintenanceConfig[] = []
 
-const maintenances: MaintenanceConfig[] = [
-  {
-    // [Optional] Monitor IDs to be affected by this maintenance
-    monitors: ['foo_monitor', 'bar_monitor'],
-    // [Optional] default to "Scheduled Maintenance" if not specified
-    title: 'Test Maintenance',
-    // Description of the maintenance, will be shown at status page
-    body: 'This is a test maintenance, server software upgrade',
-    // Start time of the maintenance, in UNIX timestamp or ISO 8601 format
-    start: '2020-01-01T00:00:00+08:00',
-    // [Optional] end time of the maintenance, in UNIX timestamp or ISO 8601 format
-    // if not specified, the maintenance will be considered as on-going
-    end: '2050-01-01T00:00:00+08:00',
-    // [Optional] color of the maintenance alert at status page, default to "yellow"
-    color: 'blue',
-  },
-]
-
-// Don't edit this line
+// Don't forget this, otherwise compilation fails.
 export { maintenances, pageConfig, workerConfig }
